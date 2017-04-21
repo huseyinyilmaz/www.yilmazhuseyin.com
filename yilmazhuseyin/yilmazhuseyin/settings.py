498f09a2e4b8022761e0fbabaa3dce65738245b4
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2017-04-20 20:35:10 -0700

    Update log file location.

--- yilmazhuseyin/yilmazhuseyin/settings.py
+++ yilmazhuseyin/yilmazhuseyin/settings.py
@@ -216,7 +216,7 @@ LOGGING = {
             # 'class': 'logging.FileHandler',
             'class': 'logging.handlers.RotatingFileHandler',
             'formatter': 'simple',
-            'filename': expanduser('~/logs/yilmazhuseyin.log'),
+            'filename': expanduser('/var/log/yilmazhuseyin.log'),
         },
 
 
