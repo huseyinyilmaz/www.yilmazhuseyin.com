13ecf23d5f76438c68ddbb56f837a280d5a54579
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2017-05-12 22:43:17 -0700

    Add sentry integration.

--- yilmazhuseyin/yilmazhuseyin/production.py
+++ yilmazhuseyin/yilmazhuseyin/production.py
@@ -1,3 +1,4 @@
+import raven
 from os.path import expanduser
 
 from yilmazhuseyin.settings import * # NOQA
@@ -18,4 +19,16 @@ CACHES = {
         },
         'KEY_PREFIX': 'blog',
     }
+}
+
+
+INSTALLED_APPS += [ # noqa
+    'raven.contrib.django.raven_compat',
+]
+
+RAVEN_CONFIG = {
+    'dsn': 'https://ce46e08f3f7e4a6f8c506c56216db533:3b9782e235db408d994323950afc97d5@sentry.io/165504',  # noqa
+    # If you are using git, you can also automatically configure the
+    # release based on the git info.
+    'release': raven.fetch_git_sha(os.path.join(os.path.dirname(os.path.dirname(os.pardir)), '..')), # noqa
 }
